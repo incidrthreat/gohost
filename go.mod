42ea54b68f346432f8100c86114db9d33ee3867a
incidrthreat <incidrthreat@gmail.com>
2021-10-02 02:35:24 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/incidrthreat/GoHost
+
+go 1.15
+
+require (
+	github.com/golang/protobuf v1.5.2
+	golang.org/x/net v0.0.0-20210929193557-e81a3d93ecf6 // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.26.0
+)
---
+++ grpc/server/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/incidrthreat/GoHost/grpc/gohost"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	Alive bool
+}
+
+func (s *server) IsAlive(ctx context.Context, in *pb.AliveRequest) {
+	log.Printf("Data Received from: %v", in.GetHostname())
+	s.Alive = in.GetAlive()
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGoHostServer{s, &server{}}
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
